7bb5ef5524c51fce2211059602f89a7ea6dcacf7
Treata11 <treata11@yahoo.com>
2024-04-02 15:47:31 +0330

    Initial Commit

---
+++ BookiShader/BookiShaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookiShaderApp.swift
+//  BookiShader
+//
+//  Created by Treata Norouzi on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BookiShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookiShader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookiShader
+//
+//  Created by Treata Norouzi on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
