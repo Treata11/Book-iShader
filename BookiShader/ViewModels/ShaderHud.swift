43b13fafe8ea8941c526fb088ee2551ad7abcfd0
Treata11 <treata11@yahoo.com>
2024-04-05 15:43:44 +0330

    created a HUD-model for shaders.

---
+++ BookiShader/ViewModels/ShaderHud.swift
@@ -0,0 +1,23 @@
+/*
+ ShaderHud.swift
+ Book iShader
+
+ Created by Treata Norouzi on 4/5/24.
+ 
+ Abstarct:
+ A WIP VM which would create a video-like HUD-model for shaders in order to play/pause
+ rewind/fast-forwad the Shaders
+*/
+
+import Observation
+
+// !!!: WIP
+
+@Observable
+class ShaderHud {
+    
+    /**
+     If the schedule should stop generating updates.
+     */
+    var paused: Bool = true
+}
