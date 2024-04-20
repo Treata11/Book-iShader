2d5bda4a17c2b9782486a1d21d28def1555167e5
Treata11 <treata11@yahoo.com>
2024-04-20 19:19:47 +0330

    Added a simple template for previewing transitions of iShader.

---
+++ BookiShader/Views/Transition/TransitionPreview.swift
@@ -0,0 +1,79 @@
+/*
+ TransitionPreview.swift
+ Book iShader
+
+ Created by Treata Norouzi on 4/17/24.
+ 
+ Abstract:
+ A simple template to for previewing transitions
+*/
+
+import Transition
+import SwiftUI
+
+struct TransitionPreview: View {
+    /// The transition which is called from a shader function
+    var transition: AnyTransition
+    /// The duration of the transition
+    var duration: TimeInterval = 1.618
+    
+    init(transition: AnyTransition, duration: TimeInterval = 1.618) {
+        self.transition = transition
+        self.duration = duration
+    }
+    
+    /// Whether we're showing the first view or the second view.
+    @State private var showingFirstView = true
+    
+    var body: some View {
+        VStack {
+            if showingFirstView {
+                firstView
+                    .transition(transition)
+
+            } else {
+                secondView
+                    .transition(transition)
+            }
+            
+            // TODO: Instead of the button, use a Timeline view which automatically transitions back & forth between the views with a slight rest
+            Button("Toggle Views") {
+                withAnimation(.easeIn(duration: duration)) {
+                    showingFirstView.toggle()
+                }
+            }
+        }
+    }
+    
+    var firstView: some View {
+        Image(.road59Chalus)
+            .resizable()
+            .aspectRatio(4/3, contentMode: .fit)
+    }
+    
+    var secondView: some View {
+        Image(.darakehTehran)
+            .resizable()
+            .aspectRatio(4/3, contentMode: .fit)
+    }
+}
+
+// MARK: - Previews
+
+#Preview("Circle") { TransitionPreview(transition: .circles()) }
+
+#Preview("swirl") { TransitionPreview(transition: .swirl()) }
+
+#Preview("Genie") { TransitionPreview(transition: .genie()) }
+
+#Preview("Wind") { TransitionPreview(transition: .wind()) }
+
+#Preview("radial") { TransitionPreview(transition: .radial) }
+
+#Preview("Crosswarp") { TransitionPreview(transition: .crosswarp(rightToLeft: true), duration: 3) }
+
+#Preview("Dreamy") { TransitionPreview(transition: .dreamy, duration: 3) }
+
+#Preview("Window Blinds") { TransitionPreview(transition: .windowBlinds(count: 66), duration: 3) }
+
+#Preview("Morph") { TransitionPreview(transition: .morph(strength: 0.1), duration: 3) }
