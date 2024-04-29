2a7783035d5c9d6e6d164c7e5c602700e10a548a
Treata11 <treata11@yahoo.com>
2024-04-29 16:10:39 +0330

    Enhancements to `ShaderPage` View.

--- BookiShader/Views/ShaderPage.swift
+++ BookiShader/Views/ShaderPage.swift
@@ -12,24 +12,47 @@ import SwiftUI
 
 // !!!: WIP
 
+// TODO: The code of the shader function could also be displayed in this page
+
 struct ShaderPage: View {
     @Environment(ShaderHud.self) var hud: ShaderHud
     
     var shaderView: AnyView
+    /// Name of the shader
     var name: String
     
+    @State private var showHUD = false
+    
     var body: some View {
-        VStack {
-            
+        VStack(alignment: .leading) {
             shaderView
+                .aspectRatio(4/3, contentMode: .fit)
+            // The HUD
+                .overlay(alignment: .bottom) {
+                    if showHUD {
+                        HUD()
+                            .transition(.blurReplace)
+//                            .transition(.offset(y: 25))
+                    }
+                }
+                .onHover { hovering in
+                    withAnimation(.easeIn(duration: 0.333)) {
+                        showHUD = hovering
+                    }
+                }
+                
+            Text(name)
+                .font(.system(.callout))
         }
-        .overlay(alignment: .bottom) {
-            HUD()
+        .onTapGesture {
+            withAnimation(.easeIn(duration: 0.333)) {
+                showHUD.toggle()
+            }
         }
     }
 }
 
 #Preview("ShaderPage") {
-    ShaderPage(shaderView: AnyView(HypnoticRipplesView()), name: "HypnoticRipples")
+    ShaderPage(shaderView: AnyView(HypnoticRipplesView()), name: "Hypnotic Ripples")
         .enhancedPreview(preferredColorScheme: .light)
 }
